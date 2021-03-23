630b2bb27a0ea8795fa425e2b8227b2a510aa9fb
xingxbai <baixingxinglive@163.com>
2021-03-23 21:19:15 +0800

    dialog 组件提交

---
+++ DialogComponent/dialog.js
@@ -0,0 +1,72 @@
+export default class Dialog extends EventTarget{
+    constructor(options) {
+        super()
+        const defaultOptions = {
+            width: '30%',
+            height: '250px',
+            content: '我是内容',
+            title: '我是标题',
+            dragable: true,
+            maskable: true,
+            isCancel: false,
+            success(){},
+            cancel(){}
+        }
+        this.opts = Object.assign(defaultOptions, options)
+        this.init()
+    }
+    init() {
+        this.createElement()
+
+        this.addEventListener('success', this.opts.success) //自定义事件
+        this.addEvent()
+    }
+    createElement() {
+        const divEles = document.createElement('div')
+        divEles.innerHTML = `<div class="k-dialog">
+            <div class="k-head">
+                <span>我是头部</span>
+            </div>
+            <div class="k-body">
+                <span>我是内容</span>
+            </div>
+            <div class="k-footer">
+                <span class="k-cancel">取消</span>
+                <span class="k-primary">确定</span>
+            </div>
+        </div>`
+        divEles.style.display = 'none'
+        document.body.appendChild(divEles)
+        this.divEles = divEles
+    }
+
+    addEvent() {
+        const kDialog = this.divEles.querySelector('.k-dialog')
+        kDialog.addEventListener('click', (e) => {
+            let className = e.target.className
+            switch (className) {
+                case 'k-close':
+                    this.close()
+                    break;
+                case 'k-cancel':
+                    this.close()
+                    this.opts.cancel()
+                    break;
+                case 'k-primary':
+                    const success = new CustomEvent('success') // 自定义事件，继承EventTarget
+                    this.dispatchEvent(success) //自定义事件
+                    this.close()
+                   // this.opts.success()
+                    break;
+                default:
+                    console.log('未命中')
+            }
+        })
+    }
+    open() {
+        this.divEles.style.display = 'block'
+    }
+    close() {
+        this.divEles.style.display = 'none'
+    }
+}
---
+++ JS/app.js
@@ -0,0 +1,6 @@
+console.log('module')
+
+export default {
+    a:1,
+    b:2
+}
